3c31d7a466cc834fea634185acec4db18b7921c5
Kavya-25 <90210074+Kavya-25@users.noreply.github.com>
2024-06-03 11:04:40 +0530

    new clients & services added

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import './Clients.scss'
+import React from "react";
+import "./Clients.scss";
 
 export const Clients = () => {
   return (
-    <div className='clients'>
-    <img src="/images/whitelogo.png" alt="" />
-    <div className="client-names">
-    <p className='heading'>Our Clients</p>
+    <div className="clients">
+      <img src="/images/whitelogo.png" alt="" />
+      <div className="client-names">
+        <p className="heading">Our Clients</p>
         <ul>
-            <li>New Tech Filter Pvt. Ltd. Himachal Pradesh</li>
-            <li>SB Infosoft Pvt. Ltd.,Himachal Pradesh</li>
-            <li>Aimil Pharmaceuticals Pvt. Ltd,Himachal Pradesh</li>
-            <li>Veeline India, Himachal Pradesh</li>
-            <li>Weikfield Foods Pvt. Ltd.,Himachal Pradesh</li>
-            <li>Elfin Drugs Pvt. Ltd.,Himachal Pradesh</li>
+          <li>New Tech Filter Pvt. Ltd. Himachal Pradesh</li>
+          <li>SB Infosoft Pvt. Ltd.,Himachal Pradesh</li>
+          <li>Aimil Pharmaceuticals Pvt. Ltd,Himachal Pradesh</li>
+          <li>Veeline India, Himachal Pradesh</li>
+          <li>Weikfield Foods Pvt. Ltd.,Himachal Pradesh</li>
+          <li>Elfin Drugs Pvt. Ltd.,Himachal Pradesh</li>
+          <li>Bio Deal Pharma,Himachal Pradesh</li>
+          <li>Onyx Biotech Pvt. Ltd.,Himachal Pradesh</li>
+          <li>Bioiv Labs Pvt. Ltd.,Nalagarh,Himachal Pradesh</li>
+          <li>Motor Appliances,Baddi,Himachal Pradesh</li>
         </ul>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
